25d9024f327c44c3feea90100446a6f02fdee1bb
Adyncunningham <alcunningha2@ualr.edu>
2026-07-28 22:17:49 +0000

    rename, fix, and background

--- js/dvd.js
+++ js/dvd.js
@@ -1,5 +1,3 @@
-
-
 const dvd = document.querySelector(".DVD");
 
 let x = 100;
@@ -7,6 +5,18 @@ let y = 100;
 
 let dx = 3; // Horizontal speed
 let dy = 3; // Vertical speed
+
+let hue = 0;
+
+function changeColor() {
+    hue += 60;
+
+    if (hue >= 360) {
+        hue = 0;
+    }
+
+    dvd.style.filter = `hue-rotate(${hue}deg)`;
+}
 
 function animate() {
     const dvdWidth = dvd.offsetWidth;
@@ -21,11 +31,13 @@ function animate() {
     // Bounce off left/right walls
     if (x <= 0 || x + dvdWidth >= screenWidth) {
         dx *= -1;
+        changeColor();
     }
 
     // Bounce off top/bottom walls
     if (y <= 0 || y + dvdHeight >= screenHeight) {
         dy *= -1;
+        changeColor();
     }
 
     dvd.style.left = x + "px";
