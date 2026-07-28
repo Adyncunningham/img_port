b3cbb359dfa24a6ae57853f2dcac75f95e4091dc
Adyncunningham <alcunningha2@ualr.edu>
2026-07-28 21:57:33 +0000

    started js

---
+++ js/dvd.js
@@ -0,0 +1,35 @@
+const dvd = document.querySelector(".DVD");
+
+let x = 100;
+let y = 100;
+
+let dx = 3; // Horizontal speed
+let dy = 3; // Vertical speed
+
+function animate() {
+    const dvdWidth = dvd.offsetWidth;
+    const dvdHeight = dvd.offsetHeight;
+
+    const screenWidth = window.innerWidth;
+    const screenHeight = window.innerHeight;
+
+    x += dx;
+    y += dy;
+
+    // Bounce off left/right walls
+    if (x <= 0 || x + dvdWidth >= screenWidth) {
+        dx *= -1;
+    }
+
+    // Bounce off top/bottom walls
+    if (y <= 0 || y + dvdHeight >= screenHeight) {
+        dy *= -1;
+    }
+
+    dvd.style.left = x + "px";
+    dvd.style.top = y + "px";
+
+    requestAnimationFrame(animate);
+}
+
+animate();
